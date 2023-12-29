6bc3e52d3841ba0de05bc9381223cdc4bc6f7e22
krzysiekk22d <klas.club@gmail.com>
2023-12-29 18:22:26 +0100

    Initial Commit

---
+++ KeyboardLanguage/KeyboardLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardLanguageApp.swift
+//  KeyboardLanguage
+//
+//  Created by Krzysztof Czura on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
